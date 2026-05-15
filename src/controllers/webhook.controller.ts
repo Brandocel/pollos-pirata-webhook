fab023bf6e15df00a8959909512d394c37cf3286
Brandocel <desarrollador@byhoka.com>
2026-05-15 13:41:09 -0500

    Fix

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -58,8 +58,8 @@ function isRecord(value: unknown): value is Record<string, unknown> {
 }
 
 function looksLikeUuid(value: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(
-    value
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+    value.trim()
   );
 }
 
